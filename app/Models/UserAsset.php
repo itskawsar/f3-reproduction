7f761e468b2f8915c3d90c9a57c107cab067962c
Kawsar Ahmad <itskawsar@gmail.com>
2023-12-07 12:54:30 +0600

    added UserAsset

---
+++ app/Models/UserAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserAsset extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * Get the company that owns the Billoflading
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/factories/UserAssetFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserAsset>
+ */
+class UserAssetFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'asset_name' => fake()->company(),
+            'user_id' => rand(1, 10),
+        ];
+    }
+}

f4caa66cdabe960e4cb6913cbb85d785dc4ffa4e
Kawsar Ahmad <itskawsar@gmail.com>
2023-12-09 01:31:23 +0600

    added  field to users table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+        ]);
+
         \App\Models\User::factory(10)->create();
         \App\Models\UserAsset::factory(100)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
